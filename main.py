ade6a70094e7c1f25faa31dca0ca4966e40e452f
AspirantDrago <sanekclubstr@mail.ru>
2019-09-25 20:29:50 +0500

    Create empty window

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pygame
+import random
 
 
+size = width, height = 400, 300
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
 
+running = True
 
+screen2 = pygame.Surface(size)
+while running:
+    for event in pygame.event.get():
+        screen2 = pygame.Surface(size)
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
