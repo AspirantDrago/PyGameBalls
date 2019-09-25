86a82388115b5e828245c7c3b11d17c70582c3d7
AspirantDrago <sanekclubstr@mail.ru>
2019-09-26 00:48:06 +0500

    Draw falling circles + optimize with second surface

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ class Ball:
         pygame.draw.circle(self.screen, self.color, self.pos, self.radius)
 
     def update(self):
-        self.pos[1] = min(self.pos[1] + self.speed / fps, height - self.radius)
-        pygame.draw.circle(self.screen, self.color, list(map(int, self.pos)), self.radius)
-        return False
+        self.pos[1] += self.speed / fps
+        if self.pos[1] < height - self.radius:
+            pygame.draw.circle(self.screen, self.color, list(map(int, self.pos)), self.radius)
+            return False
+        pygame.draw.circle(screen2, self.color, list(map(int, self.pos)), self.radius)
+        return True
 
 
 size = width, height = 400, 300
@@ -25,14 +28,14 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 speed = 100
 fps = 60
-
 running = True
 pos = 0, 0
 screen2 = pygame.Surface(size)
 balls = []
 while running:
     clock.tick(fps)
-    screen2.fill(pygame.Color(0, 0, 0), (0, 0, *size))
+    screen.fill(pygame.Color(0, 0, 0), (0, 0, *size))
+    screen.blit(screen2, (0, 0))
     for i in range(len(balls) - 1, -1, -1):
         if balls[i].update():
             del balls[i]
@@ -40,7 +43,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            balls.append(Ball(event.pos, screen2))
-    screen.blit(screen2, (0, 0))
+            balls.append(Ball(event.pos, screen))
     pygame.display.flip()
 pygame.quit()
